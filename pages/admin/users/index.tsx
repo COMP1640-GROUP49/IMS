4fcbf74b97bc472024d43df8ef199946a11f19d9
phulengo <quainhanp24@gmail.com>
2022-03-16 06:24:25 +0700

    feat(admin-users-management): add pagination & its style

--- pages/admin/users/index.tsx
+++ pages/admin/users/index.tsx
@@ -1,13 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { GetServerSideProps, NextPage } from 'next';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import ReactPaginate from 'react-paginate';
 import { Button } from 'components/Button';
 import { Header } from 'components/Header';
 import { Icon } from 'components/Icon';
 import { MetaTags } from 'components/MetaTags';
 import { UserList } from 'components/UserList';
 import { getUsersList } from 'pages/api/admin';
-import { IAccountsProps } from 'lib/interfaces';
+import { IAccountData, IAccountsProps } from 'lib/interfaces';
+import { scrollToElementByClassName } from 'utils/scrollAnimate';
 
 export const getServerSideProps: GetServerSideProps = async () => {
 	const { data } = await getUsersList();
@@ -21,6 +25,25 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
 const UsersManagement: NextPage<IAccountsProps> = ({ data: accounts }) => {
 	const { asPath } = useRouter();
+	const limit = 5;
+	const [currentItems, setCurrentItems] = useState<IAccountData[]>();
+	const [pageCount, setPageCount] = useState(0);
+	const [itemOffset, setItemOffset] = useState(0);
+
+	useEffect(() => {
+		const endOffset = itemOffset + limit;
+		setCurrentItems(accounts.slice(itemOffset, endOffset));
+		setPageCount(Math.ceil(accounts.length / limit));
+	}, [itemOffset, accounts, limit]);
+
+	const handlePageClick = (event: any) => {
+		const newOffset = (event.selected * limit) % accounts.length;
+		setItemOffset(newOffset);
+	};
+
+	const handlePaginationClick = () => {
+		scrollToElementByClassName('pagination');
+	};
 
 	return (
 		<>
@@ -38,7 +61,25 @@ const UsersManagement: NextPage<IAccountsProps> = ({ data: accounts }) => {
 						</a>
 					</Link>
 				</div>
-				<UserList accounts={accounts} />
+				<UserList accounts={currentItems} />
+				<div className="flex justify-between items-center sm:flex-col gap-6">
+					<p>
+						Showing from {itemOffset + 1} to{' '}
+						{currentItems && accounts.indexOf(currentItems[currentItems?.length - 1]) + 1} of {accounts.length} results
+					</p>
+					<ReactPaginate
+						onClick={handlePaginationClick}
+						initialPage={0}
+						className="pagination"
+						breakLabel="..."
+						nextLabel=">"
+						onPageChange={handlePageClick}
+						pageRangeDisplayed={3}
+						pageCount={pageCount}
+						previousLabel="<"
+						renderOnZeroPageCount={null!}
+					/>{' '}
+				</div>
 				{/* <Modal isShown={isShown} hide={toggle} headerText="Create Uers" modalContent={<h1>dasdadasdas</h1>} /> */}
 			</main>
 			{/* <div className="flex flex-col lgs:flex-row lg:justify-between items-start lg:items-center py-7 sm:w-full"> */}
